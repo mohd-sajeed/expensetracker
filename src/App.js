9709f65f0d4a84425528428030ea10a2713162b5
mohdsajeed <mohdsajeed871@gmail.com>
2023-08-24 20:30:10 +0530

    expensetracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import AddExpense from "./pages/add-expense";
+import Home from "./pages/home/index";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/add-expense" component={AddExpense} />
+      </Switch>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return <footer>Made with 🔥 by Sajid</footer>;
+};
+
+export default Footer;
